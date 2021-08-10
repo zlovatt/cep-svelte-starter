b6c8bb30deccc1cea9de43cd19ef11928400acdb
Zack Lovatt <zack@zacklovatt.com>
2021-08-10 09:54:09 -0700

    Fix incorrect PlugPlug subextension in other places

--- src/js/typings/framework.d.ts
+++ src/js/typings/framework.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../node_modules/types-for-adobe/shared/PlugPlugExternalObject.ts" />
+/// <reference path="../../../node_modules/types-for-adobe/shared/PlugPlugExternalObject.d.ts" />
 /// <reference path="../../typings/framework.d.ts" />
 
 class PrefsOptions {
